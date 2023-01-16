5b3e9c613cd7e9747a700f3d82f678b98f977990
Sebastian F <sebpfdev@gmail.com>
2023-01-15 19:49:29 -0500

    Create quote card UI

--- front-end/quote-machine/src/components/quote-card.tsx
+++ front-end/quote-machine/src/components/quote-card.tsx
@@ -1,3 +1,31 @@
+import './quote-card.css';
+
 export default function QuoteCard() {
-	return <div>QuoteCard</div>;
+	return (
+		<div id="quote-box">
+			<div className="quote">
+				<blockquote id="text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cupiditate ab, at
+					quo quam ratione maiores libero, nemo asperiores dolore eum quisquam harum
+					quaerat ea nisi nam tempora atque rem.
+				</blockquote>
+				<cite id="author"> - Author</cite>
+			</div>
+
+			<div className="buttons">
+				<button id="new-quote" className="btn">
+					New Quote
+				</button>
+				<a
+					id="tweet-quote"
+					className="btn"
+					// href=""
+					target="_blank"
+					rel="noreferrer"
+				>
+					Tweet
+				</a>
+			</div>
+		</div>
+	);
 }
