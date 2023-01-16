1cf264b74569c7f94a4564e07768eea7db3dbbcf
Sebastian F <sebpfdev@gmail.com>
2023-01-15 19:59:55 -0500

    Extract quote data to variable

--- front-end/quote-machine/src/components/quote-card.tsx
+++ front-end/quote-machine/src/components/quote-card.tsx
@@ -1,15 +1,17 @@
 import './quote-card.css';
 
 export default function QuoteCard() {
+	const quote = {
+		text: 'Lorem ipsum dolor sit veniam maxime facere nihil rem sapiente quidem quaerat quas error nisi nemo eius recusandae aperiam quam mollitia molestias similique. Dolor ipsam eos provident explicabo quasi non iure, consectetur, repellat inventore commodi dolores illo eligendi optio.',
+		author: 'Author',
+		tweet: `"quoted text" \n - Author`,
+	};
+
 	return (
 		<div id="quote-box">
 			<div className="quote">
-				<blockquote id="text">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cupiditate ab, at
-					quo quam ratione maiores libero, nemo asperiores dolore eum quisquam harum
-					quaerat ea nisi nam tempora atque rem.
-				</blockquote>
-				<cite id="author"> - Author</cite>
+				<blockquote id="text">{quote.text}</blockquote>
+				<cite id="author"> - {quote.author}</cite>
 			</div>
 
 			<div className="buttons">
@@ -19,7 +21,7 @@ export default function QuoteCard() {
 				<a
 					id="tweet-quote"
 					className="btn"
-					// href=""
+					href={`https://twitter.com/intent/tweet?text=${encodeURI(quote.tweet)}`}
 					target="_blank"
 					rel="noreferrer"
 				>
