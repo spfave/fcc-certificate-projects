650712f7d5e50f399effa2b1305d326133ac2c06
Sebastian F <sebpfdev@gmail.com>
2023-02-11 21:13:53 -0500

    Enable sound on click

--- front-end/drum-machine/src/components/drum-machine.tsx
+++ front-end/drum-machine/src/components/drum-machine.tsx
@@ -1,10 +1,41 @@
+type DrumSound = {
+	soundName: string;
+	soundSrc: string;
+	soundKey: string;
+};
+
+const DrumSounds: DrumSound[] = [
+	{ soundName: 'Heater 1', soundSrc: 'audio/Heater-1.mp3', soundKey: 'Q' },
+];
+
 import './drum-machine.css';
 
+// Component: Drum Machine
 export default function DrumMachine() {
 	return (
 		<div id="drum-machine">
-			<div id="drum-keys">Keys</div>
+			<div id="drum-pads">
+				<DrumButton {...DrumSounds[0]} />
+			</div>
 			<div id="display">Display</div>
 		</div>
+	);
+}
+
+// Componet: Drum Button
+type DrumButtonProps = DrumSound;
+function DrumButton(props: DrumButtonProps) {
+	const { soundName, soundSrc, soundKey } = props;
+
+	function handlePlaySound() {
+		const sound = document.getElementById(soundKey) as HTMLAudioElement;
+		sound.play();
+	}
+
+	return (
+		<button id={soundName} className="drum-pad" onClick={handlePlaySound}>
+			<audio id={soundKey} className="clip" src={soundSrc}></audio>
+			{soundKey}
+		</button>
 	);
 }
