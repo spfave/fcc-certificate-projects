aff3729d7566f910e8afc1fcf8654f07d2063b37
Sebastian F <sebpfdev@gmail.com>
2023-02-12 00:59:51 -0500

    Refactor sound data to Map struct

--- front-end/drum-machine/src/components/drum-machine.tsx
+++ front-end/drum-machine/src/components/drum-machine.tsx
@@ -9,18 +9,17 @@ type DrumSound = {
 	soundKey: string;
 };
 
-const drumSounds: DrumSound[] = [
-	{ soundName: 'Heater 1', soundSrc: 'audio/Heater-1.mp3', soundKey: 'q' },
-	{ soundName: 'Heater 2', soundSrc: 'audio/Heater-2.mp3', soundKey: 'w' },
-	{ soundName: 'Heater 3', soundSrc: 'audio/Heater-3.mp3', soundKey: 'e' },
-	{ soundName: 'Heater 4', soundSrc: 'audio/Heater-4.mp3', soundKey: 'a' },
-	{ soundName: 'Clap', soundSrc: 'audio/Heater-6.mp3', soundKey: 's' },
-	{ soundName: 'Open HH', soundSrc: 'audio/Dsc_Oh.mp3', soundKey: 'd' },
-	{ soundName: 'Kick & Hat', soundSrc: 'audio/Kick_n_Hat.mp3', soundKey: 'z' },
-	{ soundName: 'Kick', soundSrc: 'audio/RP4_KICK_1.mp3', soundKey: 'x' },
-	{ soundName: 'Closed HH', soundSrc: 'audio/Cev_H2.mp3', soundKey: 'c' },
-];
-const soundKeys = drumSounds.map(({ soundKey }) => soundKey);
+const drumSounds: Map<string, DrumSound> = new Map([
+	['q', { soundName: 'Heater 1', soundSrc: 'audio/Heater-1.mp3', soundKey: 'q' }],
+	['w', { soundName: 'Heater 2', soundSrc: 'audio/Heater-2.mp3', soundKey: 'w' }],
+	['e', { soundName: 'Heater 3', soundSrc: 'audio/Heater-3.mp3', soundKey: 'e' }],
+	['a', { soundName: 'Heater 4', soundSrc: 'audio/Heater-4.mp3', soundKey: 'a' }],
+	['s', { soundName: 'Clap', soundSrc: 'audio/Heater-6.mp3', soundKey: 's' }],
+	['d', { soundName: 'Open HH', soundSrc: 'audio/Dsc_Oh.mp3', soundKey: 'd' }],
+	['z', { soundName: 'Kick & Hat', soundSrc: 'audio/Kick_n_Hat.mp3', soundKey: 'z' }],
+	['x', { soundName: 'Kick', soundSrc: 'audio/RP4_KICK_1.mp3', soundKey: 'x' }],
+	['c', { soundName: 'Closed HH', soundSrc: 'audio/Cev_H2.mp3', soundKey: 'c' }],
+]);
 
 // Utility functions
 function playSound(soundKey: string) {
@@ -29,29 +28,33 @@ function playSound(soundKey: string) {
 	sound.play();
 }
 
-function playKeySound(event: KeyboardEvent) {
-	if (soundKeys.includes(event.key)) playSound(event.key);
-}
-
 // Component: Drum Machine
 export default function DrumMachine() {
 	const [displaySound, setDisplaySound] = useState('');
 
 	useEffect(() => {
-		window.addEventListener('keydown', playKeySound);
+		window.addEventListener('keydown', handlePlayKeySound);
 
 		// Clean up
-		return () => window.removeEventListener('keydown', playKeySound);
+		return () => window.removeEventListener('keydown', handlePlayKeySound);
 	}, []);
 
 	function handlePlaySound(sound: DrumSound) {
 		playSound(sound.soundKey);
 		setDisplaySound(sound.soundName);
 	}
+
+	function handlePlayKeySound({ key }: KeyboardEvent) {
+		if (drumSounds.has(key)) {
+			playSound(key);
+			setDisplaySound((drumSounds.get(key) as DrumSound).soundName);
+		}
+	}
+
 	return (
 		<div id="drum-machine">
 			<div id="drum-pads">
-				{drumSounds.map((drumSound) => (
+				{[...drumSounds.values()].map((drumSound) => (
 					<DrumButton
 						key={drumSound.soundKey}
 						{...drumSound}
