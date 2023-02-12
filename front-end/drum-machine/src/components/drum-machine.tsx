66277b585c508ee133c78887113ac3465c475f48
Sebastian F <sebpfdev@gmail.com>
2023-02-11 22:58:39 -0500

    Create drum button grid

--- front-end/drum-machine/src/components/drum-machine.tsx
+++ front-end/drum-machine/src/components/drum-machine.tsx
@@ -2,14 +2,23 @@ import { useEffect } from 'react';
 
 import './drum-machine.css';
 
+// Data
 type DrumSound = {
 	soundName: string;
 	soundSrc: string;
 	soundKey: string;
 };
 
-const DrumSounds: DrumSound[] = [
+const drumSounds: DrumSound[] = [
 	{ soundName: 'Heater 1', soundSrc: 'audio/Heater-1.mp3', soundKey: 'q' },
+	{ soundName: 'Heater 2', soundSrc: 'audio/Heater-2.mp3', soundKey: 'w' },
+	{ soundName: 'Heater 3', soundSrc: 'audio/Heater-3.mp3', soundKey: 'e' },
+	{ soundName: 'Heater 4', soundSrc: 'audio/Heater-4.mp3', soundKey: 'a' },
+	{ soundName: 'Clap', soundSrc: 'audio/Heater-6.mp3', soundKey: 's' },
+	{ soundName: 'Open HH', soundSrc: 'audio/Dsc_Oh.mp3', soundKey: 'd' },
+	{ soundName: 'Kick & Hat', soundSrc: 'audio/Kick_n_Hat.mp3', soundKey: 'z' },
+	{ soundName: 'Kick', soundSrc: 'audio/RP4_KICK_1.mp3', soundKey: 'x' },
+	{ soundName: 'Closed HH', soundSrc: 'audio/Cev_H2.mp3', soundKey: 'c' },
 ];
 
 // Component: Drum Machine
@@ -17,14 +26,16 @@ export default function DrumMachine() {
 	return (
 		<div id="drum-machine">
 			<div id="drum-pads">
-				<DrumButton {...DrumSounds[0]} />
+				{drumSounds.map((drumSound) => (
+					<DrumButton key={drumSound.soundKey} {...drumSound} />
+				))}
 			</div>
 			<div id="display">Display</div>
 		</div>
 	);
 }
 
-// Componet: Drum Button
+// Component: Drum Button
 type DrumButtonProps = DrumSound;
 function DrumButton(props: DrumButtonProps) {
 	const { soundName, soundSrc, soundKey } = props;
@@ -32,6 +43,8 @@ function DrumButton(props: DrumButtonProps) {
 	useEffect(() => {
 		const body = document.querySelector('body') as HTMLBodyElement;
 		body.addEventListener('keydown', handlePlaySoundKey);
+
+		// Clean up
 		return () => body.removeEventListener('keydown', handlePlaySoundKey);
 	}, []);
 
