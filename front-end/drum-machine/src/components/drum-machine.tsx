d1305352c7e1cb7713b5b448d7a338d0d5a826cf
Sebastian F <sebpfdev@gmail.com>
2023-02-11 20:27:06 -0500

    Stub out drum machine component

---
+++ front-end/drum-machine/src/components/drum-machine.tsx
@@ -0,0 +1,10 @@
+import './drum-machine.css';
+
+export default function DrumMachine() {
+	return (
+		<div id="drum-machine">
+			<div id="drum-keys">Keys</div>
+			<div id="display">Display</div>
+		</div>
+	);
+}
