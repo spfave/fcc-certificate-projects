906e118532b7cb7698612eea707adf4402d13c76
Sebastian F <sebpfdev@gmail.com>
2023-02-22 23:30:19 -0500

    Update font styling

--- front-end/calculator/src/components/calculator.tsx
+++ front-end/calculator/src/components/calculator.tsx
@@ -21,24 +21,46 @@ export default function Calculator() {
 				{output}
 			</div>
 			<div className="buttons">
-				<button id="clear">AC</button>
-				<button id="divide">/</button>
-				<button id="multiply">x</button>
-				<button id="seven">7</button>
-				<button id="eight">8</button>
-				<button id="nine">9</button>
-				<button id="subtract">-</button>
-				<button id="four">4</button>
-				<button id="five">5</button>
-				<button id="six">6</button>
+				<button className="text" id="clear">
+					AC
+				</button>
+				<button id="divide">&divide;</button>
+				<button id="multiply">&times;</button>
+				<button className="text" id="seven">
+					7
+				</button>
+				<button className="text" id="eight">
+					8
+				</button>
+				<button className="text" id="nine">
+					9
+				</button>
+				<button id="subtract">&minus;</button>
+				<button className="text" id="four">
+					4
+				</button>
+				<button className="text" id="five">
+					5
+				</button>
+				<button className="text" id="six">
+					6
+				</button>
 				<button id="add">+</button>
-				<button id="one">1</button>
-				<button id="two">2</button>
-				<button id="three">3</button>
+				<button className="text" id="one">
+					1
+				</button>
+				<button className="text" id="two">
+					2
+				</button>
+				<button className="text" id="three">
+					3
+				</button>
 				<button id="equals" onClick={evaluate}>
 					=
 				</button>
-				<button id="zero">0</button>
+				<button className="text" id="zero">
+					0
+				</button>
 				<button id="decimal">.</button>
 			</div>
 		</div>
