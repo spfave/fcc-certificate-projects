3694796105b6980035c6f9739a38547a021e89e4
Sebastian F <sebpfdev@gmail.com>
2023-03-27 20:11:50 -0400

    Add element test ids and update UI

--- front-end/pomodoro-timer/src/components/pomodoro-timer.tsx
+++ front-end/pomodoro-timer/src/components/pomodoro-timer.tsx
@@ -14,17 +14,31 @@ export default function PomodoroTimer() {
 				<h1>Pomodoro Timer</h1>
 			</div>
 			<div className="timer">
+				<div>
+					<p id="timer-label">Session</p>
+				</div>
 				<div className="timer-display">
-					<h2>XX:YY</h2>
+					<h2 id="time-left">XX:YY</h2>
+					<audio id="beep" src=""></audio>
 				</div>
 				<div className="timer-controls">
-					<button>start/pause</button>
-					<button>reset</button>
+					<button id="start_stop">start/pause</button>
+					<button id="reset">reset</button>
 				</div>
 			</div>
 			<div className="session-controls">
-				<input type="number" name="" id="" />
-				<input type="number" name="" id="" />
+				<div>
+					<p id="session-label">Session Length</p>
+					<p id="session-length">25</p>
+					<button id="session-increment">+</button>
+					<button id="session-decrement">-</button>
+				</div>
+				<div>
+					<p id="break-label">Break Length</p>
+					<p id="break-length">5</p>
+					<button id="break-increment">+</button>
+					<button id="break-decrement">-</button>
+				</div>
 			</div>
 		</div>
 	);
